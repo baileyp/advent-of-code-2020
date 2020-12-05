9c59ea231b249c24933cab89042017aae403cfa6
Peter Bailey <b33tle@gmail.com>
2020-12-05 07:33:44 -0400

    Added a timer to the app runner

--- aoc/app.py
+++ aoc/app.py
@@ -3,6 +3,7 @@ from importlib import import_module
 from io import StringIO
 from pathlib import Path
 from collections import ChainMap
+from time import perf_counter
 
 from aoc.util import FileReader
 
@@ -26,16 +27,20 @@ def run():
         solution = import_module(f"aoc.solution.day{day.zfill(2)}")
         puzzle_part = getattr(solution, f"part{part}")
 
+        print("Puzzle Answer: ", end="")
+        start = perf_counter()
         if input_is_file:
             with open(puzzle_input, 'r') as file:
                 print(puzzle_part(FileReader(file)))
         else:
             print(puzzle_part(FileReader(StringIO(puzzle_input))))
+        elapsed = perf_counter() - start
+        print(f"Solution computed in {elapsed:0.4f} seconds")
         exit(0)
     except FileNotFoundError:
         print(f"Input file {puzzle_input} not found.")
     except AttributeError:
-        print (f"Part {part} not found.")
+        print(f"Part {part} not found.")
         exit(1)
     except ModuleNotFoundError:
         print(f"Day {day} not found.")
